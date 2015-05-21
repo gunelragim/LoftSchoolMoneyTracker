4576fb0d95424c8f82ca30464314cc6167234849
gunelragim <gunelraqimova@gmail.com>
2015-05-21 15:20:15 +0500

    Fragments views

--- app/src/main/java/ru/loftschool/loftschoolmoneytracker/MainActivity.java
+++ app/src/main/java/ru/loftschool/loftschoolmoneytracker/MainActivity.java
@@ -1,26 +1,10 @@
 package ru.loftschool.loftschoolmoneytracker;
 
-import android.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ListView;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class MainActivity extends ActionBarActivity {
-
-  //  private ListView listview;
-   // private TransactionAdapter transactionAdapter;
-    //private Date dateTime;
-   // List<Transaction> data = new ArrayList<>();
 
 
     @Override
@@ -29,32 +13,6 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         getFragmentManager().findFragmentById(R.id.fragment_tramsaction);
         final Transaction_Fragment fragmentById = (Transaction_Fragment)getFragmentManager().findFragmentById(R.id.fragment_tramsaction);
-
-
-     //  List <Transaction> adapterData;
-       //adapterData = getTransactions();
-       //transactionAdapter = new TransactionAdapter(this,adapterData);
-       //listview = (ListView)findViewById(R.id.list);
-      // listview.setAdapter(transactionAdapter);
-
-
-
     }
 
-
-   // private List<Transaction> getTransactions()
-   // {
-
-
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-  //      Date now_calendar = Calendar.getInstance().getTime();
-    //    String dateToday = dateFormat.format(now_calendar);
-
-
-
-      //  data.add(new Transaction("Phone",200, dateToday));
-        //data.add(new Transaction("Meal",300, dateToday));
-        //data.add(new Transaction("Bus",300,dateToday));
-       // return data;
-//    }
 }
--- app/src/main/java/ru/loftschool/loftschoolmoneytracker/Transaction_Fragment.java
+++ app/src/main/java/ru/loftschool/loftschoolmoneytracker/Transaction_Fragment.java
@@ -2,7 +2,6 @@ package ru.loftschool.loftschoolmoneytracker;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,21 +17,19 @@ import java.util.List;
  * Created by Гюнель on 11.05.2015.
  */
 public class Transaction_Fragment extends Fragment {
-      private ListView listview;
-     private TransactionAdapter transactionAdapter;
+    private ListView listview;
+    private TransactionAdapter transactionAdapter;
     private Date dateTime;
-     List<Transaction> data = new ArrayList<>();
+    List<Transaction> data = new ArrayList<>();
 
 
-
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-     final View inflate = inflater.inflate(R.layout.fragment_tramsaction, container,false);
+        final View inflate = inflater.inflate(R.layout.fragment_tramsaction, container, false);
 
 
-        List <Transaction> adapterData = getTransactions();
+        List<Transaction> adapterData = getTransactions();
         TransactionAdapter transactionAdapter = new TransactionAdapter(getActivity(), adapterData);
         ListView listview = (ListView) inflate.findViewById(R.id.list);
         listview.setAdapter(transactionAdapter);
@@ -41,7 +38,7 @@ public class Transaction_Fragment extends Fragment {
         return inflate;
 
 
-       // return super.onCreateView(inflater, container, savedInstanceState);
+        // return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     public List<Transaction> getTransactions() {
@@ -49,9 +46,9 @@ public class Transaction_Fragment extends Fragment {
         Date now_calendar = Calendar.getInstance().getTime();
         String dateToday = dateFormat.format(now_calendar);
 
-        data.add(new Transaction("Phone",200, dateToday));
-        data.add(new Transaction("Meal",300, dateToday));
-        data.add(new Transaction("Bus",300,dateToday));
+        data.add(new Transaction("Phone", 200, dateToday));
+        data.add(new Transaction("Meal", 300, dateToday));
+        data.add(new Transaction("Bus", 300, dateToday));
         return data;
     }
 }
