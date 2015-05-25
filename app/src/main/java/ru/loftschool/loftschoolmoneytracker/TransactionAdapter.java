1c88e6686ab9e48f8606a4834f67d7799c11e6ba
gunelragim <gunelraqimova@gmail.com>
2015-05-25 18:20:13 +0500

    RecyclerView, FloatingActionBar

--- app/src/main/java/ru/loftschool/loftschoolmoneytracker/TransactionAdapter.java
+++ app/src/main/java/ru/loftschool/loftschoolmoneytracker/TransactionAdapter.java
@@ -1,40 +1,53 @@
 package ru.loftschool.loftschoolmoneytracker;
 
-import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.List;
 
-/**
- * Created by Гюнель on 04.05.2015.
- */
-public class TransactionAdapter extends ArrayAdapter<Transaction> {
-    public TransactionAdapter(Context context, List<Transaction> transactions) {
-        super(context, 0, transactions);
+public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.CardViewHolder> {
+    private final List<Transaction> transactions;
+
+    public TransactionAdapter(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+
+    @Override
+    public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+
+        return new CardViewHolder(itemView);
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public void onBindViewHolder(CardViewHolder holder, int position) {
+        Transaction transaction = transactions.get(position);
+        holder.title.setText(transaction.getTitle());
+        holder.sum.setText(String.valueOf(transaction.getSum()));
+        holder.date.setText(transaction.getDateTime());
 
-        Transaction transaction = getItem(position);
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+    }
+
+    @Override
+    public int getItemCount() {
+        return transactions.size();
+    }
+
+
+    public static class CardViewHolder extends RecyclerView.ViewHolder {
+        protected TextView title;
+        protected TextView sum;
+        protected TextView date;
+
+        public CardViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) this.itemView.findViewById(R.id.title);
+            sum = (TextView) this.itemView.findViewById(R.id.sum);
+            date = (TextView) this.itemView.findViewById(R.id.date);
         }
-
-        TextView title = (TextView) convertView.findViewById(R.id.title);
-        TextView sum = (TextView) convertView.findViewById(R.id.sum);
-        TextView date = (TextView) convertView.findViewById(R.id.date);
-
-        title.setText(transaction.getTitle());
-        sum.setText(Integer.toString(transaction.getSum()));
-        date.setText(transaction.getDateTime());
-        int color = position % 2 == 0 ? getContext().getResources().getColor(R.color.whiteColor) : getContext().getResources().getColor(R.color.backgroundGreen);
-        convertView.setBackgroundColor(color);
-
-        return convertView;
     }
 }
